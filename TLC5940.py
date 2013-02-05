382d51c96d161e947ec5cf212f5ae136545ebd13
Rogier Lodewijks <rogier@lodewijks.org>
2013-02-05 17:18:05 +0100

    Added proper fadeto function(still needs some cleanup though)

--- TLC5940.py
+++ TLC5940.py
@@ -13,28 +13,28 @@
 
 import wiringpi, spidev, bitstring, logging
 from time import sleep
+logger = logging.getLogger(__name__)
 
 class TLC5940 (object):
-  def __init__(self, numberof_TLC5940 = 1, numberof_registers = 8, spibus = 0, spidevice = 0, gsclkpin = 4, blankpin = 3, vprgpin = 5):
+  def __init__(self, numberof_TLC5940 = 1, spibus = 0, spidevice = 0, gsclkpin = 4, blankpin = 3, vprgpin = 5):
     """
     Class to control a TLC5940 LED controller. This was made for use on a Raspberry PI, in conjunction with
-    the WiringPiPython library. Probably isn't hard to get to work on other platforms as well.
+    the WiringPi-Python library. Probably isn't hard to get to work on other platforms as well.
     This version has vprg, blank en gsclock connected to GPIO's, XLAT is connected
     to the CE SPI pin. Multiple TLC5940's can be daisy chained. (soon)
     """
-    self.numberof_registers = numberof_registers
+    
     self.numberof_TLC5940 = numberof_TLC5940
     self.numberof_leds = self.numberof_TLC5940 * 16
     self.numberof_RGBleds = int(self.numberof_leds // 3)
     
     self.DCLevels = [0 for x in range(self.numberof_leds)]  #internal DC register
-    self.DCRegisters = [self.DCLevels for i in range (self.numberof_registers)]
     
-    logging.info("Configured %i TLC5940 with %i channels. (max %i RGB channels)" % (self.numberof_TLC5940, self.numberof_leds, self.numberof_RGBleds))
+    logger.info("Configured %i TLC5940 with %i channels. (max %i RGB channels)" % (self.numberof_TLC5940, self.numberof_leds, self.numberof_RGBleds))
     self.spi = spidev.SpiDev()
     self.spi.open(spibus, spidevice) #/dev/spidev0.0
     self.spi.max_speed_hz = 500000
-    logging.info("Setting SPI clock speed to %i Hz." % self.spi.max_speed_hz)
+    logger.info("Setting SPI clock speed to %i Hz." % self.spi.max_speed_hz)
     
     self.gsclkpin = gsclkpin
     self.blankpin = blankpin
@@ -45,14 +45,14 @@ class TLC5940 (object):
     self.gpio.pinMode(self.vprgpin, self.gpio.OUTPUT)
     
     #Init pins for clockless (DC, no PWM) operation only
-    logging.info("Initializing TLC5940...")
+    logger.info("Initializing TLC5940...")
     self.gpio.digitalWrite(self.blankpin, self.gpio.HIGH) #display off, reset the internal counter
     self.gpio.digitalWrite(self.gsclkpin, self.gpio.LOW) 
     self.writeAllPWM(0xfff)
     self.writeAllDC(0)
     self.gpio.digitalWrite(self.blankpin, self.gpio.LOW)  #diplay on, go counter go.
     self.gpio.digitalWrite(self.gsclkpin, self.gpio.HIGH)  #the zeroth pulse only.. :)
-    logging.info("...Let's go!")
+    logger.info("...Let's go!")
     
   def writeAllPWM(self, value = 0):
     """
@@ -82,15 +82,40 @@ class TLC5940 (object):
     Writes a given DC register to the TLC5940 IC.
     Not multi-chip capable yet!!
     """
-    register = list(input)  #make local copy
+    if len(input) == 0:
+      register = list(self.DCLevels)
+    else:
+      register = list(input)
+
     register.reverse()      #Reversal is somehow needed..
     DCdata_packed = []
-    for i in range(0,16,4): DCdata_packed += self.pack_4to3(register[i:i+4])
+    for i in range(0,len(register),4): DCdata_packed += self.pack_4to3(register[i:i+4])
 
     self.gpio.digitalWrite(self.vprgpin, self.gpio.HIGH)
     self.spi.writebytes(DCdata_packed)
 
-  def setRGB(self, offset = 0, value = []):
+  def fadeto(self, targetlevels = [], offset = 0, steps = 64, duration = 0.2):
+    """
+    Fades linearly to new value, writes to internal register and IC directly.
+    Usable for any number of LEDs, no specific colour handling.
+    Foe now no delay nor timing is implemented.
+    """
+    count = len(targetlevels)
+    if (offset + count) > self.numberof_leds:
+      logger.warning("Whoopsie.. Cannot address over %i LEDs. Truncating input." % self.numberof_leds)
+      count = self.numberof_leds - offset
+    
+    targetlevels = [self.clamp(x >> 2, 0, 63) for x in targetlevels]              #scale and boundary confinement
+    _origlevels = self.DCLevels[offset:(offset + count)]                            #Save original levels
+    _deltas = [targetlevels[i] - self.DCLevels[offset + i] for i in range(count)]   #Calculate deltas
+    
+    for i in range(steps + 1):                                                      #Do fade loop
+      scaler = float(i)/steps
+      self.DCLevels[offset:(offset + count)] = [int(_origlevels[i] + (_deltas[i] * scaler)) for i in range(count)]
+      self.writeDC(self.DCLevels)
+      sleep(duration/steps)
+
+  def setRGB(self, value = [], offset = 0):
     """
     Sets (doesn't write out to IC) the given value in the local DC register. Values in 0-255.
     """
@@ -98,41 +123,17 @@ class TLC5940 (object):
       self.DCLevels[offset:offset + 3] = [self.clamp(value[i] >> 2, 0, 63) for i in range(3)]
       return True
     else:
-      logging.warning("Whoopsie.. Cannot address over %i RGB LEDs." % self.numberof_RGBleds)
+      logger.warning("Whoopsie.. Cannot address over %i RGB LEDs." % self.numberof_RGBleds)
       return False
   
   def getRGB(self, offset = 0):
     """
-    Gets back RGB values (scaled to RGB888) as a list from internal DC register, starting from offset.
+    Returns one RGB value (scaled to RGB888) as a list from internal DC register, starting from offset.
     """
     if (offset + 3) <= self.numberof_leds:
       return [value << 2 for value in self.DCLevels[offset:offset + 3]]
     else:
-      return False
-  
-  def fadeto(self, offset = 0, target_values = [], steps = 64):
-    """
-    Fades linearly to new value, writes to internal register and IC directly.
-    Usable for any number of LEDs, no specific colour handling.
-    Foe now no delay nor timing is implemented.
-    """
-    count = len(target_values)
-    if (offset + count) <= self.numberof_leds:
-      target_values = [self.clamp(x >> 2, 0, 63) for x in target_values]              #scale and boundary confinement
-      origDClevel = self.DCLevels[offset:(offset + count)]                            #Save original levels
-      deltas = [target_values[i] - self.DCLevels[offset + i] for i in range(count)]   #Calculate deltas
-      
-      for i in range(steps + 1):                                                      #Do fade loop
-        scaler = float(i)/steps
-        self.DCLevels[offset:(offset + count)] = [int(origDClevel[i] + (deltas[i] * scaler)) for i in range(count)]
-        self.writeDC(self.DCLevels)
-        
-      sleep(2)
-      return True
-      
-    else: 
-      logging.error("Whoopsie.. Cannot address over %i LEDs." % self.numberof_leds)
-      return False
+      return []
   
   def blinkwriteAllDC(self, times):
     for y in range(times):
@@ -150,8 +151,8 @@ class TLC5940 (object):
     output bytes: 00000011 11112222 22333333
     """
     if len(inp) != 4:
-      logging.error("Input to pack_4to3 kaputt.")
-      return False
+      logger.error("Input to pack_4to3 should have 4 elements.")
+      return []
     inp = [value & 63 for value in inp] 
     return [(inp[0] << 2) & 255 | inp[1] >> 4, (inp[1] << 4) & 255 | inp[2] >> 2, (inp[2] << 6) & 255 | inp[3]]
 
@@ -161,7 +162,7 @@ class TLC5940 (object):
     output bytes: 00000000 00001111 11111111
     """
     if len(inp) != 2:
-      logging.error("Input to pack_2to3 ist kaputt.")
-      return False
+      logger.error("Input to pack_2to3 should have 2 elements.")
+      return []
     inp = [value & 4095 for value in inp] 
     return [inp[0] >> 4, (inp[0] << 4) & 255 | inp[1] >> 8, inp[1] & 255]
--- enlighten.py
+++ enlighten.py
@@ -7,29 +7,73 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-  return "Howdy parner... <a href=https://github.com/eflukx/enlightenPi>EnlightenPi</a> seems to be runnin'<br>For more information please refer to the readme."
+  return "Howdy partner... <a href=https://github.com/eflukx/enlightenPi>EnlightenPi</a> seems to be runnin'<br>For more information please refer to the readme."
 
 @app.route('/fadeto')
 def fadeto():
-    kleur = pygame.Color(str(request.args.get('color')))
-    led = int(request.args.get('led'))
-    #print 'Rood: %s, Groen: %s, Blauw: %s' % (kleur.r,kleur.g,kleur.b)
-    TLC.fadeto(led * 3, (kleur.r,kleur.g,kleur.b))
-    TLC.writeDC()
-    return 'Fading LED %s to color %s, %s, %s' % (led, kleur.r, kleur.g, kleur.b)
+  if request.args.get('duration') == None:
+    duration = 0.5
+  else:
+    duration = float(request.args.get('duration'))
+
+  if request.args.get('color') == None:
+    return "argument 'color' needs to be present"
+  else:
+    colours = [pygame.Color(str(value)) for value in request.args.get('color').split(',')]
+  
+  if request.args.get('led') == None:
+    leds = range(TLC.numberof_RGBleds)
+  else:
+    leds = [int(value) for value in request.args.get('led').split(',')]
+  
+  if len(leds) > len(colours):
+    for i in range(len(leds)-len(colours)): colours.append(colours[-1])
+    logging.info("Number of leds given exceeds colours given.")
+  
+  newRGBlevels = []
+  for value in colours:
+    newRGBlevels += [value.r, value.g, value.b]  #new RGB levels list of tuples.
+  
+  RGBlevels = [TLC.DCLevels[x] << 2 for x in range(len(TLC.DCLevels))]             #Get existing old levels (new values will overwrite)
+  
+  logging.info(", ".join(["Fading LED %i to %s" % (zpper[0], zpper[1]) for zpper in zip(leds,colours)]))
+  for i in range(len(leds)):
+    offset = leds[i] * 3 #current led offset
+    RGBlevels[offset:offset + 3] = newRGBlevels[i*3:i*3 + 3]
+    
+  print RGBlevels
+  print len(RGBlevels)
+    
+  TLC.fadeto(RGBlevels, duration = duration)
+  return "<br>".join(["Faded LED %i to %s in %s seconds" % (zpper[0], zpper[1], duration) for zpper in zip(leds,colours)])
+
+@app.route('/fadeall')
+def fadeall():
+  kleur = pygame.Color(str(request.args.get('color')))
+  color = [[kleur.r,kleur.g,kleur.b][c % 3] for c in range(16)]
+  TLC.fadeto(color)
+  return 'Fading all LEDs to color %s, %s, %s' % (kleur.r, kleur.g, kleur.b)
 
 @app.route("/test")
 def test():
-  value = int(request.args.get('value'))
-  TLC.writeAllDC(value)
-  return "Test routine executed."
+  #value = int(request.args.get('value'))
+  #TLC.writeAllDC(value)
+  print request.args.get('value')
+  if request.args.get('value') == None:
+    print "noeeoen"
+    return "value in None"
+  return "Test routine executed. Value is %s" % request.args.get('value')
   
 @app.route("/blank")
 def blank():
   TLC.writeAllDC(0)
   return "Blanked"
-
-    
+  
+@app.route("/unblank")
+def unblank():
+  TLC.writeDC(TLC.DCLevels)
+  return "Blanking disabled"
+  
 @app.route('/setled')
 def setled():
   """
@@ -48,7 +92,7 @@ def setled():
 
   logging.info(", ".join(["Setting LED %i to %s" % (value[0],value[1]) for value in zip(leds, coloursRGB)]))
   for i in range(len(leds)):
-    TLC.setRGB(leds[i] * 3, coloursRGB[i])
+    TLC.setRGB(coloursRGB[i], leds[i] * 3)
   TLC.writeDC(TLC.DCLevels)
   
   return "<br>".join(["Setting LED %i to %s" % (value[0],value[1]) for value in zip(leds, coloursRGB)])
